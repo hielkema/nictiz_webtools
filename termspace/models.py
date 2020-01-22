d538e8f194b5ab7efdb9d7f88f8f94e48f9a595d
Sander Mertens <49483781+mertenssander@users.noreply.github.com>
2020-01-22 11:26:17 +0100

    Allow empty columns in db - allow for termspace inconsisitencies

--- termspace/models.py
+++ termspace/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.utils import timezone
 
 class TermspaceComments(models.Model):
-    task_id = models.CharField(max_length=300)
-    concept = models.CharField(max_length=300)
-    fsn = models.CharField(max_length=600)
-    assignee = models.CharField(max_length=300)
-    status = models.CharField(max_length=300)
-    folder = models.CharField(max_length=600)
-    time = models.CharField(max_length=300)
+    task_id = models.CharField(max_length=300, default=None, null=True, blank=True)
+    concept = models.CharField(max_length=300, default=None, null=True, blank=True)
+    fsn = models.CharField(max_length=600, default=None, null=True, blank=True)
+    assignee = models.CharField(max_length=300, default=None, null=True, blank=True)
+    status = models.CharField(max_length=300, default=None, null=True, blank=True)
+    folder = models.CharField(max_length=600, default=None, null=True, blank=True)
+    time = models.CharField(max_length=300, default=None, null=True, blank=True)
     comment = models.TextField()
