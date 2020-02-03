004ee1daa4e7b208b16bf26f386e67adc35f0de4
Sander Mertens <49483781+mertenssander@users.noreply.github.com>
2020-02-03 18:47:50 +0100

    Test adding source/target decorator

--- termspace/views.py
+++ termspace/views.py
@@ -169,6 +169,11 @@ class Mapping_Progressreport_perProject(viewsets.ViewSet):
 
                 if tasks.exists():
                     for task in tasks:
+                        if task.project_id.project_type == "1": task_type = 'Source'
+                        if task.project_id.project_type == "2": task_type = 'Target'
+                        if task.project_id.project_type == "3": task_type = '?'
+                        if task.project_id.project_type == "4": task_type = 'Target'
+
                         output.append({
                             'id' : component.component_id,
                             'codesystem' : component.codesystem_id.codesystem_title,
@@ -176,9 +181,11 @@ class Mapping_Progressreport_perProject(viewsets.ViewSet):
                             'group' : extra.get('Groep'),
                             'AUB' : aub,
                             'actief' : extra.get('Actief?'),
+                            'soort' : extra.get('Soort'),
                             'title' : component.component_title,
                             'aantal taken' : tasks.count(),
                             'project' : task.project_id.title,
+                            'source or target' : task_type,
                             'status' : task.status.status_title,
                         })
                 else:
@@ -189,9 +196,11 @@ class Mapping_Progressreport_perProject(viewsets.ViewSet):
                             'group' : extra.get('Groep'),
                             'AUB' : aub,
                             'actief' : extra.get('Actief?'),
+                            'soort' : extra.get('Soort'),
                             'title' : component.component_title,
                             'aantal taken' : tasks.count(),
                             'project' : None,
+                            'source or target' : None,
                             'status' : None,
                         })
 
