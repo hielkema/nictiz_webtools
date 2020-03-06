3ea0d3dd09e4c1c224365ee11b3ef57cfd6aab03
Sander Mertens <49483781+mertenssander@users.noreply.github.com>
2020-03-06 17:47:13 +0100

    removed debug toolbar - not functional

--- app/urls.py
+++ app/urls.py
@@ -35,8 +35,8 @@ urlpatterns = [
     path(r'', include('homepage.urls')),
 ]
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+# if settings.DEBUG:
+#     import debug_toolbar
+#     urlpatterns = [
+#         path('__debug__/', include(debug_toolbar.urls)),
+#     ] + urlpatterns
