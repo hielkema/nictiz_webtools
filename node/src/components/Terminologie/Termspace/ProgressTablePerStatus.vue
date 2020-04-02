0ab4805f734017737bf7f61a28aeb851c9fe0be6
Sander Mertens <49483781+mertenssander@users.noreply.github.com>
2020-04-02 09:37:22 +0200

    indent

--- node/src/components/Terminologie/Termspace/ProgressTablePerStatus.vue
+++ node/src/components/Terminologie/Termspace/ProgressTablePerStatus.vue
@@ -22,28 +22,28 @@
 export default {
     components: {
     },
-  data() {
-    return {
-        loading: false,
-        headers: [
-            { text: 'Datum', value: 'date' },
-            { text: 'Semantisch / Probleem', value: 'SemanticProblem2019volkert' },
-            { text: 'Medisch', value: 'Medical2019volkert' },
-            { text: 'CAT incompleet', value: 'incompleteCAT2019volkert' },
-            { text: 'Alle taken', value: 'allTasks' },
-            { text: 'Open taken', value: 'OpenTasks' },
-        ]
-    };
-  },
-  methods: {
-        
-  },
-  computed: {
-    results() {
-      return this.$store.state.TermspaceProgress.ProgressPerStatus_table;
+    data() {
+        return {
+            loading: false,
+            headers: [
+                { text: 'Datum', value: 'date' },
+                { text: 'Semantisch / Probleem', value: 'SemanticProblem2019volkert' },
+                { text: 'Medisch', value: 'Medical2019volkert' },
+                { text: 'CAT incompleet', value: 'incompleteCAT2019volkert' },
+                { text: 'Alle taken', value: 'allTasks' },
+                { text: 'Open taken', value: 'OpenTasks' },
+            ]
+        };
+    },
+    methods: {
+            
+    },
+    computed: {
+        results() {
+        return this.$store.state.TermspaceProgress.ProgressPerStatus_table;
+        }
+    },
+    created() {
     }
-  },
-  created() {
-  }
 };
 </script>
