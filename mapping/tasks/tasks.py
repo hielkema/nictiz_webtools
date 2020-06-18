48dd3c097ab011435593b48766fb542ac5ec9f99
Sander Mertens <49483781+mertenssander@users.noreply.github.com>
2020-06-18 13:20:04 +0200

    Only spawn SNOMED dailybuild on single-task QA runs

--- mapping/tasks/tasks.py
+++ mapping/tasks/tasks.py
@@ -625,7 +625,9 @@ def audit_async(audit_type=None, project=None, task_id=None):
         send_task('mapping.tasks.qa_nhg_labcodeset.nhg_loinc_order_vs_observation', [], {'taskid':task.id})
         
         logger.info('Spawning general QA scripts for SNOMED')
-        send_task('mapping.tasks.qa_snomed.snomed_daily_build_active', [], {'taskid':task.id})
+        # Snowstorm daily build SNOWSTORM does not like DDOS - only run on individual rules.
+        if tasks.count() == 1:
+            send_task('mapping.tasks.qa_snomed.snomed_daily_build_active', [], {'taskid':task.id})
 
     # Also run legacy rules, in addition to rules split in multiple task files?
     legacy = True
