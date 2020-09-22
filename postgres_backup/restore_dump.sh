7385c64e821008422301daa5c5eacd0487900fcf
Sander Mertens <49483781+mertenssander@users.noreply.github.com>
2020-09-22 12:05:58 +0200

    Django upgrade

--- postgres_backup/restore_dump.sh
+++ postgres_backup/restore_dump.sh
@@ -1,3 +1,5 @@
+# First down / delete pgdata / up
 docker cp pg_backup_latest.bak nictiz_webtools_postgres_1:/home/backup_latest.bak
+docker volume rm nictiz_webtools_pgdata
 docker exec -it nictiz_webtools_postgres_1 psql -U postgres -f /home/backup_latest.bak postgres
 # docker exec -it nictiz_webtools_postgres_1 psql -U tickets tickets < /home/dump.sql
